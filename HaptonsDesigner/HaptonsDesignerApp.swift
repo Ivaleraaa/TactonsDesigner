eb2eb83a7267233640b037d3714a0579353c3e0a
Manon Hardy <hardy.m4n0n@gmail.com>
2025-05-07 11:45:47 +0200

    Initial Commit

---
+++ HaptonsDesigner/HaptonsDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaptonsDesignerApp.swift
+//  HaptonsDesigner
+//
+//  Created by Manon Hardy on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HaptonsDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HaptonsDesignerTests/HaptonsDesignerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaptonsDesignerTests.swift
+//  HaptonsDesignerTests
+//
+//  Created by Manon Hardy on 07/05/2025.
+//
+
+import Testing
+@testable import HaptonsDesigner
+
+struct HaptonsDesignerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
